cd4f7211429bc67ac805d0f0907d20356c8ccd4d
Johannes Titz <johannes@titz.science>
2021-04-15 16:25:59 +0200

    some improvements
    - change input to a data frame (give vector names for variables)
    - comment helper function
    - correct p-value calculation
    - format comments
    - reindent some lines

--- R/calc_contrast_aggregated.R
+++ R/calc_contrast_aggregated.R
@@ -1,23 +1,34 @@
 #' Calculate between contrast analysis from aggregated data (means, sds and ns)
 #'
-#' @param means vector of mean values for every condition
-#' @param sds vector of standard deviation values for every condition
-#' @param ns vector of sample size values for every condition
-#' @param lambdas vector of lambda values for every condition
+#' @param means named character, vector of mean values for every condition in
+#'   data
+#' @param sds named character, vector of standard deviation values for every
+#'   condition in data
+#' @param ns named character, vector of sample size values for every condition
+#'   in data
+#' @param lambdas named character, vector of lambda values for every condition
+#'   in data
 #'
-#' @return Calculates the significance of the contrast analysis.
-#  The contrastweights, the corresponding group and an effectsize are
-#' given.
+#' @return Calculates the significance of the contrast analysis. The
+#contrastweights, the corresponding group and an effectsize are ' given.
 #'
-#' @references Rosenthal, R., Rosnow, R.L., & Rubin, D.B. (2000).
-#' Contrasts and effect sizes in behmeansioral research:
-#' A correlational approach. New York: Cambridge University Press.
+#' @references Rosenthal, R., Rosnow, R.L., & Rubin, D.B. (2000). Contrasts and
+#'   effect sizes in behmeansioral research: A correlational approach. New York:
+#'   Cambridge University Press.
+#'
 #' @examples
 #'
-#' data from Betsch et al. (2010), experiment 1
 #'
-#'
-calc_contrast_aggregated <- function(means, sds, ns, lambdas){
+calc_contrast_aggregated <- function(means = "means", sds = "sds", ns = "ns",
+                                     lambdas = "lambdas", data){
+    if (!is.null(data) & is.data.frame(data)) {
+    arguments <- as.list(match.call())
+    means <- eval(arguments$means, data)
+    sds <- eval(arguments$sds, data)
+    lambdas <- eval(arguments$lambdas, data)
+  } else if (!is.null(data) & !is.data.frame(data)){
+    stop("data is not a data.frame")
+  }
   df_between <- length(means) - 1
   df_within <- sum(ns) - length(means)
   sigma_within <- sum(sds^2*ns) / (sum(ns))
@@ -35,21 +46,23 @@ calc_contrast_aggregated <- function(means, sds, ns, lambdas){
   r_contrast <- sqrt(F_contrast/(F_contrast + df_within))
   r_alerting <- sqrt(F_contrast/(F_contrast * df_between))
   N <- sum(ns)
-  p_contrast <- pf(F_contrast, df_between, df_within)
-  #data.frame(r_es = r_effect_size, N = sum(ns), n_cell_mean = mean(ns))
-  # Markus object creation
+  p_contrast <- 1 - pf(F_contrast, 1, df_within)
   sig <- c(F_contrast, p_contrast, df_contrast = 1, df_within, sigma_within,
            ss_between, ss_total = NA, N - 1)
-    r <- c(r_effectsize, r_contrast, r_alerting)
-    desc <- matrix(c(means, sds / sqrt(ns)), ncol = 2, byrow = F)
-    colnames(desc) <- c("M", "SE")
-    out_l <- list(sig, desc, lambdas, r)
-    names(out_l) <- c("sig", "desc", "lambda_between", "effects")
-    class(out_l) <- c("cofad_bw")
-    structure(out_l)
-    return(out_l)
+  r <- c(r_effectsize, r_contrast, r_alerting)
+  desc <- matrix(c(means, sds / sqrt(ns)), ncol = 2, byrow = F)
+  colnames(desc) <- c("M", "SE")
+  out_l <- list(sig, desc, lambdas, r)
+  names(out_l) <- c("sig", "desc", "lambda_between", "effects")
+  class(out_l) <- c("cofad_bw")
+  structure(out_l)
+  return(out_l)
 }
 
+#' helper function to calculate between sum of squares
+#'
+#' @param x vector of mean values
+#' @param n vector of sample sizes values
 ss <- function(x, n) {
   sum(n*(x-mean(x))^2)
 }
