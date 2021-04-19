6c8c0146f0bb9422bf6679a7b9c8d9f2f5b88b84
Johannes Titz <johannes@titz.science>
2021-04-19 09:11:24 +0200

    delete copyright info

--- R/ui.R
+++ R/ui.R
@@ -1,20 +1,3 @@
-# cofad, the mixed models special agent, is a shiny app for 2-level mixed
-# models.
-#
-# Copyright (C) 2019 Johannes Titz
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Affero General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Affero General Public License along
-# with this program. If not, see <https://www.gnu.org/licenses/>.
-
 options(shiny.autoreload = F) # for faster testing
 options(shiny.sanitize.errors = FALSE) # (handle errors manually)
 enableBookmarking("url") # not currently supported, but maybe later
