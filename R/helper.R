09c7b9244a958557f223b204e4edeeaf83dd4424
Johannes Titz <johannes@titz.science>
2021-04-20 15:37:52 +0200

    mv helper function

--- R/helper.R
+++ R/helper.R
@@ -33,3 +33,19 @@ load_data <- function(datafile){
     }
     )
 }
+
+colnames_to_tags <- function(df){
+  lapply(
+    colnames(df),
+    function(co) {
+      tag(
+        "p",
+        list(
+          class = class(df[, co]),
+          tags$span(class = "glyphicon glyphicon-move"),
+          tags$strong(co)
+        )
+      )
+    }
+  )
+}
--- R/server.R
+++ R/server.R
@@ -1,20 +1,3 @@
-
-colnames_to_tags <- function(df){
-  lapply(
-    colnames(df),
-    function(co) {
-      tag(
-        "p",
-        list(
-          class = class(df[, co]),
-          tags$span(class = "glyphicon glyphicon-move"),
-          tags$strong(co)
-        )
-      )
-    }
-  )
-}
-
 #' @importFrom shinyjs show hide
 #' @importFrom shinyalert shinyalert
 #' @noRd
