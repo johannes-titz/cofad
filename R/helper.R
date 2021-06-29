06f9a15d9450ef5c14a65553bb84512d1b74ec18
Johannes Titz <johannes@titz.science>
2021-06-29 18:25:29 +0200

    improve style, document

--- R/helper.R
+++ R/helper.R
@@ -9,16 +9,16 @@
 #' @param datafile yep, the data file to upload
 #' @return data as an R object or an error
 #' @noRd
-load_data <- function(datafile){
-  fileending <- stringr::str_match(datafile$datapath, "(\\..+$)")[1,1]
+load_data <- function(datafile) {
+  fileending <- stringr::str_match(datafile$datapath, "(\\..+$)")[1, 1]
   data <- tryCatch({
     if (fileending == ".sav") {
       data <- Hmisc::spss.get(datafile$datapath, use.value.labels = F)
     } else if (fileending == ".csv") {
       encoding <- unlist(readr::guess_encoding(datafile$datapath)[1, 1])
-      L <- readLines(datafile$datapath, n = 1)
-      numfields_semicolon <- count.fields(textConnection(L), sep = ";")
-      numfields_colon <- count.fields(textConnection(L), sep = ",")
+      lines <- readLines(datafile$datapath, n = 1)
+      numfields_semicolon <- count.fields(textConnection(lines), sep = ";")
+      numfields_colon <- count.fields(textConnection(lines), sep = ",")
       if (numfields_semicolon == 1) {
         data <- utils::read.csv(datafile$datapath, fileEncoding = encoding)
       } else if (numfields_colon == 1) {
@@ -26,15 +26,28 @@ load_data <- function(datafile){
       }
     }
     data},
-    error = function(error_message){
-      msg <- "Sorry, I could not read your data. Please check that it is in the SPSS format .sav or a regular .csv file with a comma as a separator (not a semicolon or any other delimiter)."
+    error = function(error_message) {
+      msg <- paste(
+        "Sorry, I could not read your data. Please check that it is in the ",
+        "SPSS format .sav or a regular .csv file with a comma as a separator ",
+        "(not a semicolon or any other delimiter).",
+        sep = ""
+      )
       shinyalert::shinyalert("Error", msg)
       message(error_message)
     }
-    )
+  )
 }
 
-colnames_to_tags <- function(df){
+#' Make column names to html tags
+#'
+#' This is a sortable helper that covert column names of a data frame to
+#' proper html tags for use with sortable_js
+#'
+#' @param df the data frame to convert
+#' @return html object with column names of the df
+#' @noRd
+colnames_to_tags <- function(df) {
   lapply(
     colnames(df),
     function(co) {
@@ -50,23 +63,37 @@ colnames_to_tags <- function(df){
   )
 }
 
-create_default_lambdas <- function(levels){
+#' Creates default (linear) lambdas
+#'
+#' This is used in shiny input tables to have some default.
+#'
+#' @param levels levels to create default lambdas for
+#' @return vector of lambdas
+#' @noRd
+create_default_lambdas <- function(levels) {
   lambdas <- 1:length(levels)
   lambdas <- lambdas - mean(lambdas)
   names(lambdas) <- names(levels)
   lambdas
 }
 
-create_table <- function(within_levels){
-    df <- data.frame(
-      levels = within_levels,
-      lambda = create_default_lambdas(within_levels)
-    )
-    the_tab <- rhandsontable::rhandsontable(
-      df,
-      stretchH = "all",
-      rowHeaders = NULL
-    )
-    # make first column read only
-    rhandsontable::hot_col(the_tab, "levels", readOnly = T)
-    }
+#' Creates rhandsontable for lambda input
+#'
+#' Used in shiny to create a UI for setting lambda values.
+#'
+#' @param levels levels to create default lambdas for
+#' @return rhandsontable with lambda levels and default (linear) lambda values
+#' @noRd
+create_table <- function(levels) {
+  df <- data.frame(
+    levels = levels,
+    lambda = create_default_lambdas(levels)
+  )
+  the_tab <- rhandsontable::rhandsontable(
+    df,
+    stretchH = "all",
+    rowHeaders = NULL
+  )
+  # make first column read only
+  rhandsontable::hot_col(the_tab, "levels", readOnly = T)
+}
