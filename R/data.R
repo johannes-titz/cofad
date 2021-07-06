8dce6d968cf41f317153fbaeff13158a9473653f
Johannes Titz <johannes@titz.science>
2021-07-06 15:10:48 +0200

    add new files for data

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' Empathy data set by Furr (2004)
+#'
+#' fictious data set on empathy ratings of students from different majors
+#'
+#' @format a data frame with 20 rows and 2 columns
+#' \describe{
+#'   \item{empathy}{Empathy rating}
+#'   \item{major}{major of student}
+#' }
+#'
+#' @source Furr, R. M. (2004). Interpreting effect sizes in contrast analysis.
+#'   Understanding Statistics, 3, 1–25.
+#'   https://doi.org/10.1207/s15328031us0301_1
+"furr_p4"
+
+#' Children data set by Rosenthal and Rosnow (2000)
+#'
+#' Table 5.3 in Rosenthal and Rosnow (2000) on p. 129.
+#'
+#' @format a data frame with 36 rows and 4 columns
+#' \describe{
+#'   \item{dv}{dependent variable}
+#'   \item{between}{age group (8, 10, 12 years)}
+#'   \item{ID}{unique identifier for child}
+#'   \item{within}{measurement (1, 2, 3, 4)}
+#' }
+#'
+#' @source Rosenthal, R., Rosnow, R. L., & Rubin, D. B. (2000). Contrasts and
+#'   Effect Sizes in Behavioral Research: A Correlational Approach. Cambridge
+#'   University Press.
+"rosenthal_tbl59"
+
+#' Data set by Rosenthal and Rosnow (2000)
+#'
+#' Table 3.1 in Rosenthal and Rosnow (2000) on p. 38.
+#'
+#' @format a data frame with 20 rows and 2 columns
+#' \describe{
+#'   \item{dv}{dependent variable}
+#'   \item{between}{group (A, B, C, D))}
+#' }
+#'
+#' @source Rosenthal, R., Rosnow, R. L., & Rubin, D. B. (2000). Contrasts and
+#'   Effect Sizes in Behavioral Research: A Correlational Approach. Cambridge
+#'   University Press.
+"rosenthal_tbl31"
+
