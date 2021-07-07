58b97e64cab4852943b91e436bbc6c0a8c1a5325
Johannes Titz <johannes@titz.science>
2021-07-07 15:36:57 +0200

    document additional data sets

--- R/data.R
+++ R/data.R
@@ -45,3 +45,56 @@
 #'   University Press.
 "rosenthal_tbl31"
 
+#' Data set by Rosenthal and Rosnow (2000)
+#'
+#' Table 5.9 in Rosenthal and Rosnow (2000)
+#'
+#' @format a data frame with 12 rows and 4 columns
+#' \describe{
+#'   \item{id}{unique identifier}
+#'   \item{dv}{dependent variable}
+#'   \item{med}{within variable}
+#'   \item{pt}{between variable}
+#' }
+#'
+#' @source Rosenthal, R., Rosnow, R. L., & Rubin, D. B. (2000). Contrasts and
+#'   Effect Sizes in Behavioral Research: A Correlational Approach. Cambridge
+#'   University Press.
+"rosenthal_tbl59"
+
+#' Music data set by Sedlmeier & Renkewitz (2018)
+#'
+#' Example 16.6, table 16.5 in Sedlmeier & Renkewitz (2018). Fictious data set
+#' with 8 participants that listened to no music, white noise, classical music,
+#' and jazz music (within). The DV is a reading test.
+#'
+#' @format a data frame with 32 rows and 3 columns
+#' \describe{
+#'   \item{reading_test}{dependent variable}
+#'   \item{participant}{unique id}
+#'   \item{music}{within variable}
+#' }
+#'
+#' @source Sedlmeier, P., & Renkewitz, F. (2018). Forschungsmethoden und
+#'   Statistik für Psychologen und Sozialwissenschaftler (3rd ed.). Pearson
+#'   Studium.
+"sedlmeier_p537"
+
+#' Problem solving data set by Sedlmeier & Renkewitz (2018)
+#'
+#' Example 16.2, table 16.1 in Sedlmeier & Renkewitz (2018). Fictious data set
+#' with 15 boys divided into three groups (no training, boys-specific material,
+#' girls-specific traning material). The DV is the number of solved problem
+#' (similar to the training).
+#'
+#' @format a data frame with 15 rows and 3 columns
+#' \describe{
+#'   \item{lsg}{dv, number of svoled exercises}
+#'   \item{between}{group, KT=no training, JT=boys-specific, MT=girls-specific}
+#'   \item{lambda}{lambdas used for this example}
+#' }
+#'
+#' @source Sedlmeier, P., & Renkewitz, F. (2018). Forschungsmethoden und
+#'   Statistik für Psychologen und Sozialwissenschaftler (3rd ed.). Pearson
+#'   Studium.
+"sedlmeier_p525"
