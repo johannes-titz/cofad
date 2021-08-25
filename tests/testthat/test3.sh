2aa2aa47587c5c2ba7835295ef7f40439c2c2ce1
Johannes Titz <johannes@titz.science>
2021-08-25 09:46:12 +0200

    improve position of clicks for testing gui

--- tests/testthat/test3.sh
+++ tests/testthat/test3.sh
@@ -4,11 +4,12 @@ Rscript -e "shiny::runApp(cofad::run_app(), port = 1234)" &
 sleep 3
 xdotool exec firefox
 sleep 2
+xdotool key Control_L+l
 xdotool type 'localhost:1234'
 xdotool key "Return"
-sleep 3
+sleep 4
 # start furr test ----
-xdotool mousemove 67 240 click 1
+xdotool mousemove 67 230 click 1
 sleep 2
 xdotool key Control_L+l
 xdotool type '/home/jt/programming/cofad/data-raw/furr_p4.csv'
@@ -29,7 +30,7 @@ xdotool key "Return"
 sleep 1
 
 # start rosenthal tbl 53 test ----
-xdotool mousemove 67 240 click 1
+xdotool mousemove 67 230 click 1
 sleep 2
 xdotool key Control_L+l
 xdotool type '/home/jt/programming/cofad/data-raw/sedlmeier_p537.csv'
@@ -49,7 +50,7 @@ xdotool key "Return"
 sleep 1
 
 # start test ----
-xdotool mousemove 67 240 click 1
+xdotool mousemove 67 230 click 1
 sleep 2
 xdotool key Control_L+l
 xdotool type '/home/jt/programming/cofad/data-raw/rosenthal_tbl53.csv'
