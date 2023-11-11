6554ea29e3b3bc6d490bd601c69a4e1b39292dd0
Johannes Titz <johannes@titz.science>
2023-11-11 14:01:00 +0100

    docs: add package.Rd

--- R/summary_methods.R
+++ R/summary_methods.R
@@ -6,23 +6,26 @@
 #' @export
 summary.cofad_bw <- function(object, ...) {
   x <- object
+  s <- x$sig
+  # @Markus: please put names before calculations!
+  # round everything to 3 digits, except p value, which shows 3 sig digits
   f_tab <- matrix(c(
-    round(x[[1]][1] * x[[1]][5], 3), round(x[[1]][3]),
-    round(x[[1]][1] * x[[1]][5], 3), round(x[[1]][1], 3),
-    round(x[[1]][2], 3),
-    round(x[[1]][5] * x[[1]][4], 3), round(x[[1]][4]),
-    round(x[[1]][5], 3), NA, NA,
-    round(x[[1]][7], 3), round(x[[1]][8]),
-    NA, NA, NA),
+    # first row, kontrast
+    round(s[c("ss_kontrast", "df_contrast", "ss_kontrast", "f_contrast")], 3),
+      signif(s["p_contrast"], 3),
+    # second row within
+    round(c(s[c("ss_within", "df_inn", "ms_within")], NA, NA,
+    s[c("ss_total", "df_total")], NA, NA, NA), 3)),
     byrow = T, ncol = 5)
   rownames(f_tab) <- c("contrast", "within", "total")
   colnames(f_tab) <- c("SS", "df", "MS", "F", "p")
-  r_tab <- round(matrix(c(x[[4]]), ncol = 1), 3)
-  rownames(r_tab) <- c("r_effectsize", "r_contrast", "r_alerting")
+  r_tab <- as.matrix(round(x$effects, 3))
   colnames(r_tab) <- c("effects")
   out <- list(f_tab, r_tab)
-  names(out) <- c("F-Table", "Effects")
-  return(out)
+  names(out) <- c("FTable", "Effects")
+  warning <- ifelse(s["L"] < 0, "Attention! Your contrast is negative, meaning that it fits in the opposite direction of your lambdas!", "")
+  cat(paste0("Contrast Analysis Between\n\nL=", s["L"], warning, "\n\n", collapse = ""))
+  return(print(out, na.print = ""))
 }
 #' Summary of within subject design contrast analysis
 #' @param object output of calc_contrast
@@ -32,6 +35,7 @@ summary.cofad_bw <- function(object, ...) {
 #' @export
 summary.cofad_wi <- function(object, ci = .95) {
   x <- object
+  # @Markus: please put names before calculation!
   l_mean <- x[[2]][[1]]
   l_se <- x[[2]][2]
   l_df <- x[[1]][3]
@@ -39,7 +43,7 @@ summary.cofad_wi <- function(object, ci = .95) {
   l_se_ci <- qt(p = (1 - ci) / 2, df = l_df, lower.tail = F) * l_se
   l_upper_bound <- l_mean + l_se_ci
   l_lower_bound <- l_mean - l_se_ci
-  l_vals <- c(l_mean, l_se, l_df, x$sig[1], l_p, l_lower_bound, l_upper_bound)
+  l_vals <- c(l_mean, l_se, l_df, s$sig[1], l_p, l_lower_bound, l_upper_bound)
   l_vals <- signif(matrix(l_vals, ncol = length(l_vals)), 4)
   l_eff <- signif(matrix(c(x[[4]][1], x[[4]][2])), 3)
   rownames(l_eff) <- c("r-contrast", "g-contrast")
@@ -57,6 +61,7 @@ summary.cofad_wi <- function(object, ci = .95) {
 #' @export
 summary.cofad_mx <- function(object, ...) {
   x <- object
+  # @Markus: please put names before calculation!
   all_l <- as.vector(x[[6]])
   all_l <- all_l[which(!is.na(all_l))]
   ss_total <- sum(
