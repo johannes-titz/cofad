7512a396350d528f629e540ded1254a0cd416010
Johannes Titz <johannes@titz.science>
2023-12-04 17:49:27 +0100

    tests: migrate to shinytest2

--- tests/testthat/test-gui.R
+++ tests/testthat/test-gui.R
@@ -1,16 +1,64 @@
-context("gui examples")
-library(testthat)
+library(shinytest2)
+modify_hot_table <- function(row, col, value, table) {
+  paste0(
+    "var $table = HTMLWidgets.getInstance(", table, ").hot;",
+    "$table.setDataAtCell(", row - 1, ", ", col - 1, ", ", value, ");"
+  )
+}
 
-test_that("gui works for standard cases", {
-  skip_on_ci()
-  # run bash script
-  system("bash test3.sh", wait = TRUE)
+test_that("gui between works", {
+  start <- run_app()
+  app <- AppDriver$new(start, name = "between")
+  app$upload_file(datafile = "sedlmeier_p525.csv") # change path
+  app$set_inputs("between_name" = "between")
 
-  furr <- readLines("furr.html")
-  sedlmeier <- readLines("sedlmeier537.html")
-  rosenthal <- readLines("rosenthal_tbl53.html")
+  app$run_js(modify_hot_table(1, 2, 60, "hot_lambda_between"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(2, 2, -40, "hot_lambda_between"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(3, 2, -20, "hot_lambda_between"))
+  app$wait_for_idle()
 
-  expect_true(any(grepl("6.154", furr)))
-  expect_true(any(grepl("5.269", sedlmeier)))
-  expect_true(any(grepl("20.211", rosenthal)))
+  tbl <- app$get_value(output = "table_region")
+
+  expect_true(grepl("6.519", tbl))
+  expect_true(grepl("0.59", tbl))
+})
+
+test_that("gui within works", {
+  app <- AppDriver$new(run_app())
+  app$upload_file(datafile = "sedlmeier_p537.csv") # change path
+
+  app$set_inputs("within_name" = "music", "id_name" = "participant")
+  app$run_js(modify_hot_table(1, 2, -0.75, "hot_lambda_within"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(2, 2, -0.75, "hot_lambda_within"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(3, 2, 0.25, "hot_lambda_within"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(4, 2, 1.25, "hot_lambda_within"))
+  app$wait_for_idle()
+
+  tbl <- app$get_value(output = "table_region")
+  # sedlmeier 5.27
+  expect_true(grepl("5.269", tbl))
+})
+
+test_that("gui mixed works", {
+  app <- AppDriver$new(run_app())
+  app$upload_file(datafile = "rosenthal_tbl53.csv")
+  app$set_inputs("within_name" = "within", "id_name" = "id",
+                 "between_name" = "between")
+  app$run_js(modify_hot_table(1, 2, 0, "hot_lambda_between"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(2, 2, 1, "hot_lambda_between"))
+  app$wait_for_idle()
+  app$run_js(modify_hot_table(3, 2, -1, "hot_lambda_between"))
+  app$wait_for_idle()
+  # within is linear
+
+  tbl <- app$get_value(output = "table_region")
+  # original vaue: 20.19
+  expect_true(grepl("20.211", tbl))
+  expect_true(grepl("0.871", tbl))
 })
